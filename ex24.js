23e164cf95f33ca40c3cef5d2ca0e9079637b605
Suelle Silva <suellesilva91@gmail.com>
2025-01-30 01:51:54 -0300

    Funções e Procedimentos em JS

---
+++ ex24.js
@@ -0,0 +1,32 @@
+let num1,num2,num1t,num2t, somaT,subt,mult1,div;
+
+num1 = prompt("digite o número");
+num2 = prompt("digite o número");
+
+num1t = parseInt(num1);
+num2t = parseInt(num2);
+
+
+function soma(a,b){
+    alert("resultado da soma,subtração,multiplicação e divisão:")
+    somaT= a + b;
+    subt1 = a - b;
+    mult1 = a * b;
+    div = a / b ;
+
+
+
+     return somaT 
+
+}
+soma(num1t, num2t);
+alert(somaT);
+soma(num1,num2);
+alert(subt1)
+soma(num1,num2);
+alert(mult1)
+soma(num1,num2)
+alert(div)
+
+
+
