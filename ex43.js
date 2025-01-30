fe6c53159ea76917ef2bfa0e6bad9ac33b74548a
Suelle Silva <suellesilva91@gmail.com>
2025-01-30 15:39:23 -0300

    Objeto OOP

---
+++ ex43.js
@@ -0,0 +1,14 @@
+class livro{
+   construtor(titulo,autor, ano){
+    this.titulo = titulo
+    this.autor = autor
+    this.ano = ano 
+
+   }
+
+}
+const livro1 = new ("Faca na Garganta", "Hermes Leal", "1998");
+const livro2 = new ("100 anos de solidão", "Gabriel Garcia Marques", "1967");
+
+console.log(livro1);
+console.log(livro2);
