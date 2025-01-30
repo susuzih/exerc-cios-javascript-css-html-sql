cf7e2de06b08557a73463211bfd6d0a3a49acfc0
Suelle Silva <suellesilva91@gmail.com>
2025-01-30 02:10:25 -0300

    Alterar estilos

---
+++ ex31.js
@@ -0,0 +1,9 @@
+function Alterar(){
+ let body = document.getElementById("entrar")
+ let bodyestilo = document.getElementById("body")
+  bodyestilo.style.backgroundColor= "lightblue";  
+}
+   
+  
+
+
