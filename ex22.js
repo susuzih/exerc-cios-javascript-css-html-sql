5b250646acec4c236962d25ed8a6123dde27a05c
Suelle Silva <suellesilva91@gmail.com>
2025-01-30 01:06:33 -0300

    While no JS

---
+++ ex22.js
@@ -0,0 +1,18 @@
+const senha = "2255";
+let inteiro ;
+
+
+while(inteiro !== senha) {
+    inteiro = prompt("Digite a senha:");
+
+    if(inteiro === senha){
+     alert("senha correta! bem vindo!");
+
+    } else {
+     alert("senha incorreta. tente novamente!");
+    }
+}
+
+
+
+    
