f31a71391cd0a9cfd4e3272ce0228c26a4decc6d
Suelle Silva <suellesilva91@gmail.com>
2025-01-30 01:01:15 -0300

    Switch no JS

---
+++ ex20.js
@@ -0,0 +1,36 @@
+let semanaDia =0;
+
+semanaDia = prompt ("Digite o dia da semana :");
+
+switch (semanaDia) {
+    case "1":
+        alert("Hoje é Domingo")
+    break;
+    
+    case "2":
+       alert("hoje é segunda")
+    break;
+
+    case "3":
+        alert("hoje é terça")
+    break;
+
+    case "4":
+        alert("hoje é quarta")
+    break;
+
+    case "5":
+        alert("hoje é quinta")
+    break;
+
+    case "6":
+        alert("hoje é sexta")
+    break;
+
+    case "7":
+        alert("hoje é sábado")
+
+    default:
+        alert("default")
+        break;
+}
